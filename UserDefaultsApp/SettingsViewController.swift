e21bacb08152c576eb84d635ba6a35bd2738f4b4
Pritam Dash <pritamdash2015@outlook.com>
2022-06-11 22:43:20 +0530

    UserDefaulta added.

--- UserDefaultsApp/SettingsViewController.swift
+++ UserDefaultsApp/SettingsViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet var colorSegment : UISegmentedControl!
+    @IBOutlet var nameTextField : UITextField!
+    @IBOutlet var colorChoiceLabel : UILabel!
+    @IBOutlet var saveButton : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,5 +29,25 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func saveSettings() {
+        let name = nameTextField.text
+        let colorPosition = colorSegment.selectedSegmentIndex
+        //instead of saving the color name we will save the color index.
+        
+        //open userDefault and save name and color position.
+        
+        let userDefaults = UserDefaults.standard
+        //here we are ot using any intialiser just a static property of UserDefaults
+        
+        //After opening the userdefaults we need to set the values in it.
+        userDefaults.set(colorPosition, forKey: "color")
+        userDefaults.set(name, forKey: "userName")
+        
+        userDefaults.synchronize()  //File opens, file gets stored and file closes. It is parmanent data.
+        
+        //Inorder to close the screen.
+        self.navigationController?.popViewController(animated: true)
+    }
 
 }
